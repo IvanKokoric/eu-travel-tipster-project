3eeb9de9b38805f2031d92df8290fce5910ddf36
IvanKokoric <ikokoric@tvz.hr>
2021-10-30 20:06:53 +0200

    Dodavanje tablice

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/oruga-next/dist/oruga.css'
 
-createApp(App).mount('#app')
+createApp(...).use(Oruga);
